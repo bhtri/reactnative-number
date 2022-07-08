a72b4e0540c3132f8e12eee70169eb93a6443c91
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2022-07-09 01:38:56 +0900

    ⚡️ Background

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StatusBar} from 'react-native';
+import styles from './App.components.style';
+
+import background from './assets/image/bg.png';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar hidden = {true}/>
+      <ImageBackground
+        source={background}
+        style={styles.backgroundImage}
+      >
+
+      </ImageBackground>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
